5b986d79b8c2d9d9a20d616c37252974199f41db
André Huttel <andreluizhuttel@gmail.com>
2022-10-04 14:31:16 -0300

    alterações Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image-nvpc"></div>
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
@@ -28,27 +28,27 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" name="email" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required autofocus>
+                                        <input type="email" class="form-control form-control-user" name="email" placeholder="{{ __('E-Mail') }}" value="{{ old('email') }}" required autofocus>
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" name="password" placeholder="{{ __('Password') }}" required>
+                                        <input type="password" class="form-control form-control-user" name="password" placeholder="{{ __('Senha') }}" required>
                                     </div>
 
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
                                             <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+                                            <label class="custom-control-label" for="remember">{{ __('Lembrar-me') }}</label>
                                         </div>
                                     </div>
 
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            {{ __('Login') }}
+                                            {{ __('Entrar') }}
                                         </button>
                                     </div>
 
-                                    <hr>
+                                    {{-- <hr>
 
                                     <div class="form-group">
                                         <button type="button" class="btn btn-github btn-user btn-block">
@@ -66,7 +66,7 @@
                                         <button type="button" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> {{ __('Login with Facebook') }}
                                         </button>
-                                    </div>
+                                    </div> --}}
                                 </form>
 
                                 <hr>
@@ -74,14 +74,14 @@
                                 @if (Route::has('password.request'))
                                     <div class="text-center">
                                         <a class="small" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Password?') }}
+                                            {{ __('Esqueceu a senha?') }}
                                         </a>
                                     </div>
                                 @endif
 
                                 @if (Route::has('register'))
                                     <div class="text-center">
-                                        <a class="small" href="{{ route('register') }}">{{ __('Create an Account!') }}</a>
+                                        <a class="small" href="{{ route('register') }}">{{ __('Criar uma conta!') }}</a>
                                     </div>
                                 @endif
                             </div>
